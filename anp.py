bcd58fd2ea7aa4c3785f5e72827dacfc0afe8fdd
Alexander Feodorov <webmaster@unixcomp.org>
2022-01-24 01:50:59 +0300

    Refactoring

--- anp.py
+++ anp.py
@@ -1,17 +1,5 @@
 import numpy as np
 
-cw = np.array([ 3, 7, 5, 1, 9 ])
-cwSiz = len(cw);
-
-# Оценки по критериям уложены по строкам
-aw = np.array([
-  [3, 1, 8, 4, 5],
-  [1, 2, 5, 8, 9],
-  [5, 6, 9, 7, 2],
-  [4, 9, 7, 8, 5],
-  [9, 1, 6, 5, 3]
-])
-
 def geo_mean(iterable):
     a = np.array(iterable)
     return a.prod()**(1.0/len(a))
@@ -32,13 +20,14 @@ def eigenVec(m, dst=None):
   for i in range(rows):
     for k in range(cols):
       colsSum[i] += m[k, i]
-    colsSum[i] *= rowsGeom[i];
+    colsSum[i] *= rowsGeom[i]
   lmbda = np.sum(colsSum)
   if (lmbda > rows):
     raise "Wrong lambda"
   if (not dst is None):
     np.copyto(dst, rowsGeom)
   return rowsGeom
+
 
 def eigVecCalc(srcVec, dstVec):
   srcSiz = len(srcVec)
@@ -49,24 +38,45 @@ def eigVecCalc(srcVec, dstVec):
   eigenVec(result, dstVec)
 
 
-# normVectors = np.zeros([cwSiz + 1, cwSiz])
-eigNormVectors = np.zeros([cwSiz + 1, cwSiz]);
+def AHPSolve(normVectors, solveVector):
+  awRows, awCols = np.shape(aw)
+  # Заполняем нормирпованные собственные вектора оценок альтернатив
+  for i in range(awRows):
+    eigVecCalc(aw[i], normVectors[i + 1])
+  for i in range(awRows):
+    solveVector[i] = 0
+    for j in range(awCols):
+      solveVector[i] += normVectors[0, j] * normVectors[j+1, i]
+  minimum = np.amin(solveVector)
+  return np.where(solveVector == minimum)
+
+
+
+
+
+
+cw = np.array([ 3, 7, 5, 1, 9 ])
+cwSiz = len(cw);
+
+# Оценки по критериям уложены по строкам
+aw = np.array([
+  [3, 1, 8, 4, 5],
+  [1, 2, 5, 8, 9],
+  [5, 6, 9, 7, 2],
+  [4, 9, 7, 8, 5],
+  [9, 1, 6, 5, 3]
+])
+awRows, awCols = np.shape(aw)
+
+
+eigNormVectors = np.zeros([awCols + 1, cwSiz])
+ahpSolveVector = np.zeros([awCols])
 
 # Нормированный собственный вектор оценки критериев
 eigVecCalc(cw, eigNormVectors[0])
 
-# Заполняем нормирпованные собственные вектора оценок альтернатив
-awRows, awCols = np.shape(aw)
-for i in range(awRows):
-  eigVecCalc(aw[i], eigNormVectors[i + 1])
+result = AHPSolve(eigNormVectors, ahpSolveVector)
 
-ahpSolveVector = np.zeros([awCols])
-for i in range(awRows):
-  ahpSolveVector[i] = 0
-  for j in range(awCols):
-    ahpSolveVector[i] += eigNormVectors[0, j] * eigNormVectors[j+1, i]
-minimum = np.amin(ahpSolveVector)
-result = np.where(ahpSolveVector == minimum)
 
 print(result)
 
